b73ec85b0b8ec6fbf8336328b13fca0149b19e7e
marcus@NOTEBOOK <marcus11.04@web.de>
2020-04-07 05:51:31 +0200

    256. Disapatching Actions

--- redux-basic.js
+++ redux-basic.js
@@ -8,7 +8,28 @@ const initialState = {
 };
 
 // Reducer
+/**
+ *
+ * @param {*} state
+ * @param {*} action
+ * @Note  you have to copy it first too, so that you never mutate any data.
+ * Always do this immutable !
+ * @returns an updated Counter or an updated state with an updated counter
+ * upon the INC_COUNTER action here.
+ */
 const rootReducer = (state = initialState, action) => {
+  if (action.type === "INC_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "ADD_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + action.value,
+    };
+  }
   return state;
 };
 // Store
@@ -16,5 +37,12 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Dispatching Action
+/**
+ * @dispatch needs to have a "type" property. Nameconvention for type: "UPPERCASE STRING"
+ * getting the information which type of action was dispatched
+ */
+store.dispatch({ type: "INC_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
+console.log(store.getState());
 
 // Subscription
