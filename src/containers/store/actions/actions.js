d660d452aaeef12a6aebc2c7381b3fce412672d4
marcus@NOTEBOOK <marcus11.04@web.de>
2020-04-17 13:27:40 +0200

    289 handling async with action creators

--- src/containers/store/actions/actions.js
+++ src/containers/store/actions/actions.js
@@ -45,6 +45,8 @@ export const subtract = (value) => {
   };
 };
 /**
+ * typically case for create asynchronous actions, with a synchronous action creator,
+ * which in the end dispatch actions created by synchronous ones!
  * @store_result action creator function for synchronous code.
  * @returns actions.type: STORE_RESULT
  */
@@ -54,11 +56,19 @@ export const saveResult = (res) => {
     result: res,
   };
 };
+/**
+ * simulate that prior to this action reached out to a server to store it there..
+ * and only update our state once this one's successful, for example..
+ *@dispatch reference for dispatch some actions asynchronously inside setTimeout()
+ * this will executed by redux-thunk lib.package
+ * @param {*} res
+ * @returns action.type: saveResult() -> actually updates the state and the store
+ */
 export const storeResult = (res) => {
   return (dispatch) => {
     setTimeout(() => {
       dispatch(saveResult(res));
-    }, 2000);
+    }, 40000);
   };
 };
 
